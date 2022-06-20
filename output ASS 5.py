679ee9a259be42509e66b6487dcd1871d36c0965
abhishekbt21 <104352282+abhishekbt21@users.noreply.github.com>
2022-06-20 11:36:55 +0530

    Add files via upload

---
+++ output ASS 5.py
@@ -0,0 +1,242 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Acer\AppData\Local\Programs\Python\Python310\Assignment 5.py
+
+Enter the string to be printed in reverse order :
+python
+
+The reversed order of string entered is :
+nohtyp
+
+
+Enter the initial number of range :
+4
+
+Enter the last(ending) number of range :
+8
+
+Enter the divisor for entered range :
+2
+
+Numbers divisile in the range are :
+1 . 4
+
+2 . 6
+
+
+Enter the lenght of 1st side of triangle whose area is to be calculated :
+5
+
+Enter the lenght of 2nd side of triangle whose area is to be calculated :
+6
+
+Enter the lenght of 3rd side of triangle whose area is to be calculated :
+8
+
+Area of triangle whose sides are entered is : 14.981238266578634
+
+
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+enter no of rows to be printed:5
+A
+BC
+DEF
+GHIJ
+KLMNO
+Please, Enter the Lowest Range Value: 50
+Please, Enter the Upper Range Value: 102
+The Prime Numbers in the range are: 
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+101
+The numbers are divisible by only 7 not 11
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+84
+91
+98
+105
+112
+119
+126
+133
+140
+147
+161
+168
+175
+182
+189
+196
+203
+210
+217
+224
+238
+245
+252
+259
+266
+273
+280
+287
+294
+301
+315
+322
+329
+336
+343
+350
+357
+364
+371
+378
+392
+399
+406
+413
+420
+427
+434
+441
+448
+455
+469
+476
+483
+490
+497
+The numbers are only divisible by only 11 not 7
+11
+22
+33
+44
+55
+66
+88
+99
+110
+121
+132
+143
+165
+176
+187
+198
+209
+220
+242
+253
+264
+275
+286
+297
+319
+330
+341
+352
+363
+374
+396
+407
+418
+429
+440
+451
+473
+484
+495
+The numbers are divisible by both 7 and 11
+77
+154
+231
+308
+385
+462
+
+Enter the 1st number to be checked :
+45
+Enter the 2nd number to be checked :
+523
+Enter the 3rd number to be checked :
+-85
+Enter the 4th number to be checked :
+-14
+Enter the 5th number to be checked :
+-12
+Enter the 6th number to be checked :
+45
+Enter the 7th number to be checked :
+78
+Enter the 8th number to be checked :
+12
+Enter the 9th number to be checked :
+35
+Enter the 10th number to be checked :
+852
+
+45 is a positive as well as odd number
+
+523 is a positive as well as odd number
+
+-85 is a negative as well as odd number
+
+-14 is a negative as well as even number.
+
+-12 is a negative as well as even number.
+
+45 is a positive as well as odd number
+
+78 is a positive as well as even number.
+
+12 is a positive as well as even number.
+
+35 is a positive as well as odd number
+
+852 is a positive as well as even number.
+
+The following input numbers are odd :
+ , 45 , 523 , -85 , 45 , 35
+
+The following input numbers are positive :
+ , 45 , 523 , 45 , 78 , 12 , 35 , 852
+
+The following input numbers are negative :
+ , -85 , -14 , -12
+
+The following input numbers are even :
+ , -14 , -12 , 78 , 12 , 852
+
+Enter the number to be checked for number of appearences :
+45
+The number appeared 2 times.
+enter a string:python is a programming language
+{'python': 1, 'is': 1, 'a': 1, 'programming': 1, 'language': 1}
